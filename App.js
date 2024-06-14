c6dcd518efd821ccb49e5b9c8a87c9a370eb9dc5
Rohitramansingh <86155945+Rohitramansingh@users.noreply.github.com>
2024-06-14 18:27:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -8,8 +8,14 @@ import { Provider, useDispatch, useSelector } from "react-redux";
 import Details from "./components/Details";
 import BasicModal from "./components/CartModal";
 import Login from "./components/Login";
-import LoginModal from "./components/Loginmodal";
-import MyComponent from "./components/Api";
+import Checkout from "./components/Checkout";
+import Myorder from "./components/Myorder";
+import Profile from "./components/Myprofile";
+import Edit from "./components/Edit";
+import Sinup from "./components/Singup";
+import Singup from "./components/Singup";
+import BasicMenu from "./components/DropDown";
+// import LoginModal from "./components/Loginmodal";
 
 function App() {
   return (
@@ -17,11 +23,17 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <BasicModal />
-        <LoginModal/>
+        {/* <LoginModal/> */}
 
         <Routes>
-        <Route path="/" element={<MyComponent />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/myorder" element={<Myorder />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/singup" element={<Singup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/menu" element={<BasicMenu />} />
           {/* <Route path="/dialog" element={<Dialog />}></Route> */}
           <Route path="/details/:id" element={<Details />}></Route>
         </Routes>
